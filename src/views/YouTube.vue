af51c02f1c347f7fd3d2c9112b1080e144617974
Bernard Borg <bernard.borg36@gmail.com>
2023-09-18 01:54:50 +0200

    Added button to youtube search

--- src/views/YouTube.vue
+++ src/views/YouTube.vue
@@ -2,13 +2,16 @@
     <div id="main-div">
         <h1 style="font-weight: 600">Search video</h1>
         <X color="white" style="position: absolute; top: 10px; right: 10px" @click="closeWindow" />
-        <input
-            v-model="search"
-            id="youtube-search"
-            ref="youtubeSearch"
-            placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-            @keyup="getVideo"
-        />
+        <div style="display: flex; align-items: center">
+            <input
+                v-model="search"
+                id="youtube-search"
+                ref="youtubeSearch"
+                placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                @keyup="getVideo"
+            />
+            <Search @click="downloadVideo" color="#252526" style="margin-left: -40px; cursor: pointer; z-index: 99" />
+        </div>
         <span id="time-notice" v-if="loadingText">This will take a while</span>
         <span id="loading-text" v-if="loadingText">{{ loadingText }}</span>
     </div>
@@ -19,13 +22,14 @@ import { ref, onUnmounted, onMounted } from "vue";
 import { invoke } from "@tauri-apps/api";
 import { WebviewWindow, getCurrent } from "@tauri-apps/api/window";
 import { listen } from "@tauri-apps/api/event";
-import { useIntervalFn } from "@vueuse/core";
-import { X } from "lucide-vue-next";
+import { useIntervalFn, useOnline } from "@vueuse/core";
+import { X, Search } from "lucide-vue-next";
 
-const youtubeSearch = ref<HTMLInputElement | null>(null);
 const search = ref<string>("");
 const loadingText = ref<string>("");
 const searchFailed = ref<boolean>(false);
+
+const online = useOnline();
 
 const closeWindow = async () => {
     await WebviewWindow.getByLabel("youtube")?.close();
@@ -62,35 +66,36 @@ const keyUpEventHandler = async (event: KeyboardEvent) => {
     }
 };
 
-const getVideo = async (event: KeyboardEvent) => {
-    if (!youtubeSearch.value) {
+const downloadVideo = async () => {
+    searchFailed.value = false;
+
+    // Prevents user from clicking enter multiple times
+    if (loadingText.value) {
         return;
     }
 
-    searchFailed.value = false;
+    // Cannot download video if offline
+    if (!online.value) {
+        return;
+    }
 
+    invoke("download_video", { url: search.value });
+    resume();
+
+    const unlisten = await listen("search_failed", (event) => {
+        const error = (event.payload as { errorMessage: string }).errorMessage;
+        console.log(error);
+        pause();
+
+        searchFailed.value = true;
+    });
+
+    unlisten();
+};
+
+const getVideo = (event: KeyboardEvent) => {
     if (event.key === "Enter") {
-        // Prevents user from clicking enter multiple times
-        if (loadingText.value) {
-            return;
-        }
-
-        invoke("download-video", { url: search.value });
-        resume();
-
-        const unlisten = await listen("search-failed", (event) => {
-            const error = (event.payload as { errorMessage: string }).errorMessage;
-            console.log(error);
-            pause();
-
-            if (!youtubeSearch.value) {
-                return;
-            }
-
-            searchFailed.value = true;
-        });
-
-        unlisten();
+        downloadVideo();
     }
 };
 
