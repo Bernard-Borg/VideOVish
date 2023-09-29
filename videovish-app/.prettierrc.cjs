1f3a4e60700dc5e7b7aaecd13b419a51a2976075
Bernard Borg <bernard.borg36@gmail.com>
2023-09-30 01:18:42 +0200

    Further restructuring

--- videovish-app/.prettierrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    endOfLine: "auto",
-    tabWidth: 4,
-    trailingComma: "none",
-    semi: true,
-    singleQuote: false,
-    bracketSpacing: true,
-    printWidth: 120
-};
