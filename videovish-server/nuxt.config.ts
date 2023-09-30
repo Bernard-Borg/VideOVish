2596e522778595941b8e32d8ea02ec38c99e4c08
Bernard Borg <bernard.borg36@gmail.com>
2023-09-30 22:39:52 +0200

    Fixed CORS issues with server

--- videovish-server/nuxt.config.ts
+++ videovish-server/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: ["nuxt-security"],
+    security: {
+        corsHandler: {
+            origin: "*",
+            methods: ["GET", "POST"],
+            allowHeaders: "*"
+        }
+    },
     runtimeConfig: {
         GOOGLE_API_KEY: process.env.NUXT_GOOGLE_API_KEY,
         API_KEY: process.env.NUXT_VIDEOVISH_API_KEY
