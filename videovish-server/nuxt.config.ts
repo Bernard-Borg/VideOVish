16a5c6f21ec2f4e5d58ea3d7ee8b1c1d6ad13f2c
Bernard Borg <bernard.borg36@gmail.com>
2024-08-23 23:14:13 +0200

    Updated videovish-server dependencies

--- videovish-server/nuxt.config.ts
+++ videovish-server/nuxt.config.ts
@@ -1,26 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        head: {
-            meta: [{ name: "google-site-verification", content: "_wSxbjEo1coYAcGSykHsVB6cW54Vf2DehrRxToKw6sc" }]
-        }
-    },
-    modules: ["nuxt-security"],
-    security: {
-        corsHandler: {
-            origin: "*",
-            methods: ["GET", "POST"],
-            allowHeaders: "*"
-        }
-    },
-    runtimeConfig: {
-        GOOGLE_API_KEY: process.env.NUXT_GOOGLE_API_KEY,
-        API_KEY: process.env.NUXT_VIDEOVISH_API_KEY
-    },
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {}
-        }
-    }
+  app: {
+      head: {
+          meta: [{ name: "google-site-verification", content: "_wSxbjEo1coYAcGSykHsVB6cW54Vf2DehrRxToKw6sc" }]
+      }
+  },
+
+  modules: ["nuxt-security"],
+
+  security: {
+      corsHandler: {
+          origin: "*",
+          methods: ["GET", "POST"],
+          allowHeaders: "*"
+      }
+  },
+
+  runtimeConfig: {
+      GOOGLE_API_KEY: process.env.NUXT_GOOGLE_API_KEY,
+      API_KEY: process.env.NUXT_VIDEOVISH_API_KEY
+  },
+
+  postcss: {
+      plugins: {
+          tailwindcss: {},
+          autoprefixer: {}
+      }
+  },
+
+  compatibilityDate: "2024-08-23"
 });
